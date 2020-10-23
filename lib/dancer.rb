d260480ae4501d002d92e6d146a9e58e84f550c6
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-23 20:04:31 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,6 +2,6 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  extend FancyDance::ClassMethods 
-  include FancyDance::InstancMethods 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstancMethods
 end
