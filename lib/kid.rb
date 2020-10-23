8e31f706fc2df7de6931a8b78197da10bc00b5ce
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-23 20:05:00 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,6 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend FancyDance::ClassMethods 
+  extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
